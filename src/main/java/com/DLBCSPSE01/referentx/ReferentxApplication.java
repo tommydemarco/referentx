6113980fac10aa6f8eefe896fcc74f990353f7cf
Tommaso De Marco <tommaso.demarco95@gmail.com>
2024-10-25 22:54:47 +0200

    Initial commit - project setup

---
+++ src/main/java/com/DLBCSPSE01/referentx/ReferentxApplication.java
@@ -0,0 +1,13 @@
+package com.DLBCSPSE01.referentx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferentxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferentxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DLBCSPSE01/referentx/ReferentxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DLBCSPSE01.referentx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReferentxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
